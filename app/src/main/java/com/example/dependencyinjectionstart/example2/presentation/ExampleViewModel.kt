db50d7e06be1d6fcdc5bac62025a215db0d2ff48
ilya <ilja.kasatckin@yandex.ru>
2023-03-10 00:31:08 +0300

    Set factory like singleton. Change create to work with Provider. Create activity2 for test.

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
@@ -9,6 +10,7 @@ class ExampleViewModel @Inject constructor(
 ) : ViewModel() {
 
     fun method() {
-        useCase()
+        //useCase()
+        Log.d("ExampleViewModel", "$this")
     }
 }
